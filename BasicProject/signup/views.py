a01c8b559204239fcf0c7b4b6d042757150dcb27
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-04-05 10:24:32 +0700

    update basic project

--- BasicProject/signup/views.py
+++ BasicProject/signup/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def Index(request):
-    return HttpResponse("Hello")
+    context = {
+        
+    }
+    return render(request, 'signup/signup.html', context)
 
 def view_404_error(request, exception):
     return render(request, '404.html')
