64a1cc5b491f603167be7b06ba6c0bbde366794a
HaoHuynh <hao152903@gmail.com>
2021-04-06 18:54:01 +0700

    commit

--- BasicProject/signup/models.py
+++ BasicProject/signup/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length = 100, null = False, primary_key = True)
+    email= models.CharField(max_length = 100, null=True)
+    phone = models.CharField(max_length = 12)
+    
+    def __str__(self):
+        return self.name
