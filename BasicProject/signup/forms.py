64e1c8f3231b22bc0c4c08e602b9a3dde9859e0c
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-04-07 17:47:53 +0700

    modelForm

---
+++ BasicProject/signup/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from . import models
+
+class signUpForm(ModelForm):
+    class Meta:
+        model = models.Customer
+        fields = ['name', 'email', 'phone']
+        
