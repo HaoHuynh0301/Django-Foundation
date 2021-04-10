d4cf058366002e91b75aa5c366a17ff207a8e978
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-04-10 18:42:08 +0700

    Blog Project final commition

--- BlogProject/home/models.py
+++ BlogProject/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+	postID = models.CharField(max_length = 10, primary_key = True)
+	title = models.CharField(max_length = 30, null = False)
+	content = models.CharField(max_length = 3000)
+	date = models.DateTimeField(auto_now  = True)
+
+	def __str__(self):
+		return self.title
