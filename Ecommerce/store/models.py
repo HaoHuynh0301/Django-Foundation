363f397f7696bfa198e9221050aad739a7291ecf
HaoHuynh <hao152903@gmail.com>
2021-04-14 00:34:01 +0700

    create function for cart, checkout

--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -37,11 +37,31 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total
+
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total
+	
+	
+
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete = models.SET_NULL, null = True)
 	order = models.ForeignKey(Order, on_delete = models.SET_NULL, null = True)
 	quantity = models.IntegerField(default = 0, blank = True, null = True)
 	date_addded = models.DateTimeField(auto_now_add = True)
+
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
+	
 
 class ShippingAddress(models.Model):
 	Customer = models.ForeignKey(Customer, on_delete = models.SET_NULL, null = True)
--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -10,8 +10,31 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-	return render(request, 'store/cart.html')
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = models.Order.objects.get_or_create(customer = customer, complete = False)
+		items = order.orderitem_set.all()	
+	else:
+		order = {'get_cart_total': 0, 'get_cart_items': 0}
+		items = []
+
+	context = {'items': items, 'order': order}
+
+	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-	return render(request, 'store/checkout.html')
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = models.Order.objects.get_or_create(customer = customer, complete = False)
+		items = order.orderitem_set.all()
+	else:
+		order = {'get_cart_total': 0, 'get_cart_items': 0}
+		items = []
+
+	context = {
+		'items': items,
+		'order': order
+	}
+
+	return render(request, 'store/checkout.html', context)
 
