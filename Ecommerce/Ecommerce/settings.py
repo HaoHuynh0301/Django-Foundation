3d59ae57d2b0b1464d38f9af6a4a8a764ca8fc2f
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-04-13 18:34:47 +0700

    displaying list of products with its image

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -118,6 +118,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 
 
 def store(request):
-	return render(request, 'store/store.html')
+	products = models.Product.objects.all()
+	context = {'products': products}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
 	return render(request, 'store/cart.html')
 
 def checkout(request):
 	return render(request, 'store/checkout.html')
+
